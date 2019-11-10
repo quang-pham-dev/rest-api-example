10a34cd2d68aed3346a5fb1b90adc1a81915afea
Benkate101290 <quangpn101290@gmail.com>
2019-11-10 08:56:51 +0700

    Done

---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+// middlewares/auth.js
+
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, "longer-secret-is-better");
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Authentication failed!" });
+    }
+};
+
+// Get User Profile
+router.route('/user-profile/:id').get(authorize, (req, res, next) => {
+    userSchema.findById(req.params.id, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.status(200).json({
+                msg: data
+            })
+        }
+    })
+});
